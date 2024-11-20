9076d73be74058270a105c4d9fd7710074b3c96c
anna <50797880+matela-anya@users.noreply.github.com>
2024-11-20 22:23:33 +0300

    Create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4">Все переводы</h1>
+      <div className="grid gap-4">
+        {/* Here will be NovelCard components */}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
