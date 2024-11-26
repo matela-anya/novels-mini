71f0765d780eacb5acfa36001df72e042127a44c
anna <50797880+matela-anya@users.noreply.github.com>
2024-11-26 18:18:54 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,19 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+    proxy: {
+      '/api': {
+        target: process.env.APP_URL || 'http://localhost:3000',
+        changeOrigin: true,
+      }
+    }
   },
   build: {
     outDir: 'dist',
     sourcemap: true,
+  },
+  define: {
+    'process.env.TELEGRAM_BOT_TOKEN': JSON.stringify(process.env.TELEGRAM_BOT_TOKEN),
+    'process.env.APP_URL': JSON.stringify(process.env.APP_URL)
   }
 })
