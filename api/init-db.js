9355e720c954804affdd1356b3481a873f25ec4c
anna <50797880+matela-anya@users.noreply.github.com>
2024-11-28 00:46:42 +0300

    Update init-db.js

--- api/init-db.js
+++ api/init-db.js
@@ -1,9 +1,5 @@
 import { sql } from '@vercel/postgres';
 
-export const config = {
- runtime: 'edge',
-};
-
 export default async function handler(request) {
  if (request.method !== 'POST') {
    return new Response('Method not allowed', { 
