3a2bc51dfadd72f86e2b280eb3235e92beadd485
anna <50797880+matela-anya@users.noreply.github.com>
2025-01-02 00:51:54 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
+
+// Загружаем переменные из .env
+dotenv.config()
 
 export default defineConfig({
   plugins: [react()],
