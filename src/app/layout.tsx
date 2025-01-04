72c150faaee79270fa21d68c8b4aaa16f79ec575
anna <50797880+matela-anya@users.noreply.github.com>
2025-01-04 18:54:32 +0300

    Create layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import '@/styles/globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin', 'cyrillic'] })
+
+export const metadata: Metadata = {
+  title: 'Переводы корейских новелл',
+  description: 'Читайте переводы корейских новелл',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="ru">
+      <head>
+        <script src="https://telegram.org/js/telegram-web-app.js"></script>
+      </head>
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
